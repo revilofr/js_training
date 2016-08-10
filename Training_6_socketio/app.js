d046d9d93878d87241d9eba59ad67066afa4dc75
Olivier Abrard <olivier.abrard@gmail.com>
2016-08-10 13:23:10 +0200

    Moving the twitter app caller into its own directory so it can be shared easily. Adding a dedicated README.md for that part. Renaming into app.js

---
+++ Training_6_socketio/app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by revilo on 09/08/2016.
+ */
+
+/**
+ * The following lines allow to create both socketio app and express. Both of them will have their requests dispatched
+ * to the same http server. In other words, socketio and request are sharing the same http server.
+ */
+var express = require ('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){
+    console.log('Client connected');
+
+    client.on('messages', function(data){
+        console.log(data);
+    });
+
+    // Emit the messages event on the client (Browser)
+    client.emit('messages', { hello: 'world'});
+});
+
+
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
